37ed3db1f258e0d386652cd5feb7933891cb881b
20tsange1 <20tsange1@gmail.com>
2024-12-31 17:50:31 +0000

    Fixing a few UI issues and interactions

---
+++ tool-analysis/analysis.py
@@ -0,0 +1,54 @@
+from tree_sitter import Language, Parser
+
+class Analysis:
+    """
+    Analysis class is being developed to store and integrate different types of analysis into the IDE.
+
+    The idea is to make it as modular as possible, allowing different analysis to be added into the IDE
+    dynamically, such that as the field grows and more langauges / analysis are created, the IDE remains
+    flexible.
+
+    Furthermore, there will need to be a structural check for a grammar, as a grammar requires the analysis
+    grammar (grammar suitable for the analysis) as a subset of itself. Otherwise, the analysis will not
+    be able to evaluate the contract correctly. Furthermore, there needs to be additional consideration
+    to cases where there may be a mixture of grammars within a singular grammar, and how an analysis may
+    have to extract or handle just a portion of the contract.
+
+    We also want to keep analysis and parsing separate, making the two individual components of the IDE.
+    (Improves modularity)
+
+
+    Types of Analysis
+        - Completeness
+        - Consistency
+        - Vagueness
+    """
+
+    def __init__(self):
+        pass
+
+    def error_analyser(self, parse_tree):
+
+        # Example of an analyser, this one just counts the number of errors / total number of nodes
+        # Maybe swap to number of words, bytes
+
+        count = 0
+        total = 0
+        # cursor = self.parse_tree.root_node.walk()
+
+        if parse_tree != None:
+            s = [parse_tree.root_node]
+
+            while s:
+                node = s.pop()
+
+                if node.child_count > 0:
+                    for child in node.children:
+                        s.append(child)
+
+                if node.type == "ERROR":
+                    count += 1
+                
+                total += 1
+
+        return int((1 - (count / (total))) * 100) if total > 0 else 0
---
+++ tool-visualisations/visualiser.py
@@ -0,0 +1,36 @@
+from tree_sitter import Language, Parser
+from drawTree import DrawTree
+from drawFile import DrawFile
+
+class Visualiser:
+    def __init__(self):
+        self.types = ['Tree', 'File']
+
+        self.tree_drawer = DrawTree()
+        self.file_drawer = DrawFile()
+
+    """
+    As an idea, why not use treecursor to draw subtrees, limiting the visual representation
+
+    https://tree-sitter.github.io/py-tree-sitter/classes/tree_sitter.TreeCursor.html
+
+    https://tree-sitter.github.io/tree-sitter/using-parsers#walking-trees-with-tree-cursors
+
+    "You can access every node in a syntax tree using the TSNode APIs described above, but if 
+    you need to access a large number of nodes, the fastest way to do so is with a tree cursor. 
+    A cursor is a stateful object that allows you to walk a syntax tree with maximum efficiency.
+    
+    Note that the given input node is considered the root of the cursor, and the cursor cannot 
+    walk outside this node, so going to the parent or any sibling of the root node will return false. 
+    This has no unexpected effects if the given input node is the actual root node of the tree, but is 
+    something to keep in mind when using nodes that are not the root node."
+
+    What this means is that we are restricted to our node and it's children, perfect for our usecase.
+    
+    """
+
+    def drawTree(self, tree, width, height):
+        return self.tree_drawer.buildTree(tree, width, height)
+
+    def drawFile(self, tree):
+        return self.file_drawer.buildFileStructure(tree)
